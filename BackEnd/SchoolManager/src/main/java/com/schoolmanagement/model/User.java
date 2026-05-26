4b83d0f1e0bc4d7a6234e30fa068ca680c48639a
Kacper Zatoński <240844@edu.p.lodz.pl>
2026-05-26 22:26:44 +0200

    commit

--- BackEnd/SchoolManager/src/main/java/com/schoolmanagement/model/User.java
+++ BackEnd/SchoolManager/src/main/java/com/schoolmanagement/model/User.java
@@ -49,7 +49,8 @@ public class User implements UserDetails {
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
-
+    
+    @Builder.Default
     private boolean enabled = true;
 
     @PrePersist
